cc9cf76eaf39c61b6c56470cbd386966d7bf0c4e
Sasha <lonagi22@gmail.com>
2020-12-28 02:25:22 +0200

    read data

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -27,3 +27,39 @@ total_calories_per_week = total_calories_per_day*7
 percentage_prot = 0.3
 percentage_carb = 0.5
 percentage_fat = 0.2
+
+cal_prot = round(percentage_prot * total_calories_per_week)
+cal_carb = round(percentage_carb * total_calories_per_week)
+cal_fat = round(percentage_fat * total_calories_per_week)
+
+
+# In[3]:
+
+
+#goal grams
+gram_prot = cal_prot / prot_cal_p_gram
+gram_carb = cal_carb / carb_cal_p_gram
+gram_fat = cal_fat / fat_cal_p_gram
+
+
+# In[4]:
+
+
+products = pd.read_csv("food.csv")
+
+
+# In[5]:
+
+
+products
+
+
+# In[6]:
+
+
+cal_data = products[['Gram_Prot', 'Gram_Fat', 'Gram_Carb']]
+
+prot_data = list(cal_data['Gram_Prot'])
+fat_data = list(cal_data['Gram_Fat'])
+carb_data = list(cal_data['Gram_Carb'])
+
